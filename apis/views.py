1882bf7714747cdb14380367ed2abb70f8ae131a
Bhagyashree_nm_ <106364694+Bhagyashree2k2@users.noreply.github.com>
2023-01-16 19:30:41 +0530

    Update views.py

--- apis/views.py
+++ apis/views.py
@@ -26,16 +26,14 @@ class DetailView(APIView):
 
     def get(self, request):
         try:
-            # fetching token sent in request header by the user.
             us = Token.objects.filter(user=request.user)
-            if(us):          # checking for authentication using token authentication.
-
-                # getting user from in-built user model class.
+            if(us):         
+                
                 user = User.objects.filter(auth_token=us[0]).first()
-                # getting student from student model by filtering based on user that we got.
+                
                 details = Student.objects.get(user=user)
                 serializer = api_ser.DetailSerializer(
-                    details, context={'request': request})       # Serializing the data into Json format.
+                    details, context={'request': request})      
                 return Response({'data': serializer.data, }, status=status.HTTP_200_OK)
             else:
                 return Response({'message': 'User not authenticated'}, status=status.HTTP_400_BAD_REQUEST)
@@ -44,23 +42,19 @@ class DetailView(APIView):
 
 
 class AttendanceView(APIView):
-    """
-    This view is used to return user's attendance 
-    that is to check user's attendance.
-    """
     permission_classes = [IsAuthenticated, ]
 
     def get(self, request):
         try:
             token = Token.objects.filter(user=request.user).first()
-            if(token):  # checking for authentication using token authentication.
-                # getting user from in-built user model class.
+            if(token): 
+                
                 user = User.objects.get(auth_token=token)
-                # getting student from student model by filtering based on user that we got.
+                
                 stud = Student.objects.get(user=user)
-                # using ass_list and att_list we get the classes assigned to that user
+               
                 ass_list = Assign.objects.filter(class_id_id=stud.class_id)
-                # and respectively their attendance
+               
                 att_list = []
                 for ass in ass_list:
                     try:
@@ -71,32 +65,28 @@ class AttendanceView(APIView):
                         a.save()
                     att_list.append(a)
                 serializer = api_ser.AttendanceSerializer(
-                    att_list, many=True, context={'request': request})     # Serializing the data into Json format.
+                    att_list, many=True, context={'request': request})    
                 return Response({'user_attendance': serializer.data, }, status=status.HTTP_200_OK)
             else:
-                # returning not authenticated message when user isn't authenticated with status code 400.
+               
                 return Response({'message': 'User not authenticated'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
 
 class MarksView(APIView):
-    """
-    This view is used to return user's marks 
-    that is to check user's marks in different subjects as given by the teacher.
-    """
     permission_classes = [IsAuthenticated, ]
 
     def get(self, request):
         try:
             token = Token.objects.filter(user=request.user).first()
-            if(token):  # checking for authentication using token authentication.
+            if(token): 
                 user = User.objects.get(auth_token=token)
                 stud = Student.objects.get(user=user)
 
-                # using ass_list and sc_list we retrieve all the subjects assigned
+               
                 ass_list = Assign.objects.filter(class_id_id=stud.class_id)
-                # and then their respective marks. Store them in a dictionary and return it to the user.
+               
                 sc_list = []
                 for ass in ass_list:
                     sc = StudentCourse.objects.get(
@@ -114,23 +104,18 @@ class MarksView(APIView):
 
 
 class TimetableView(APIView):
-    """
-    This view is used to check user's class timetable
-    It returns the respective class' timetable to which the user is assigned.
-    """
-
     permission_classes = [IsAuthenticated, ]
 
     def get(self, request):
         try:
             token = Token.objects.filter(user=request.user).first()
-            if(token):  # checking for authentication using token authentication.
+            if(token):
                 user = User.objects.get(auth_token=token)
                 stud = Student.objects.get(user=user)
                 asst = AssignTime.objects.filter(
                     assign__class_id=stud.class_id)
                 serializer = api_ser.TimeTableSerializer(
-                    asst, many=True, context={'request': request})     # Serializing the data into Json format.
+                    asst, many=True, context={'request': request})    
                 return Response({'user_marks': serializer.data, }, status=status.HTTP_200_OK)
             else:
                 return Response({'message': 'User not authenticated'}, status=status.HTTP_400_BAD_REQUEST)
